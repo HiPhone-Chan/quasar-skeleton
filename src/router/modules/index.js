c5802548f355f41a1053355c8b23014ba7dac8a3
chf <hi.phone.chan@gmail.com>
2022-08-02 17:38:29 +0800

    dynamic import routes

--- src/router/modules/index.js
+++ src/router/modules/index.js
@@ -1,7 +1,9 @@
-// import
+const routerModules = import.meta.globEager('./**.js')
+const routers = []
 
+Object.keys(routerModules).forEach(key => {
+  routers.push(routerModules[key].default)
+})
 // modules entry
 /** when your routing map is too long, you can split it into small modules **/
-export default [
-
-]
+export default routers
