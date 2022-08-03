383f0efb9b40437045f217bf2789d50d7b692f98
chf <hi.phone.chan@gmail.com>
2022-08-03 16:52:16 +0800

    i18n翻译

--- src/i18n/zh-CN/index.js
+++ src/i18n/zh-CN/index.js
@@ -1,5 +1,14 @@
 
 export default {
-  failed: '失败',
-  success: '成功'
+  route: {
+    home: '首页',
+    profile: '我的中心'
+  },
+  login: {
+    title: '登录',
+    logIn: '登录',
+    username: '用户名',
+    password: '密码',
+    mobile: '手机'
+  }
 }
