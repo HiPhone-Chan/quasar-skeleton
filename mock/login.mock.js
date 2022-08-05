2fe415d545258603e617f213f228ce451b40b741
Crystal <zhaoxiaobin@zhaoxiaobindeMacBook-Air.local>
2022-08-05 14:46:58 +0800

    接口逻辑

--- mock/login.mock.js
+++ mock/login.mock.js
@@ -1,11 +1,17 @@
-const data = require('./data/account.data.json')
+const data = require("./data/account.data.json");
 
 module.exports = [
   {
     url: `${process.env.VUE_APP_BASE_API}/api/authenticate`,
-    method: 'POST',
-    response: () => {
-      return data.token
-    }
-  }
-]
+    method: "POST",
+    response: ({ body }) => {
+      for (let i = 0; i < data.length; i++) {
+        const login = data[i].account.login;
+        if (body.username === login) {
+          return data[i].token;
+        }
+      }
+      return null;
+    },
+  },
+];
