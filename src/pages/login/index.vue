974b99b516cf105f6d3f6480aeb38940e5e9e37e
chf <hi.phone.chan@gmail.com>
2022-08-15 12:28:46 +0800

    完善骨架页面

--- src/pages/login/index.vue
+++ src/pages/login/index.vue
@@ -1,26 +1,19 @@
 <template>
-  <div class="column" style="height: 200px">
-    <div class="col">
-    </div>
-    <div class="col self-center">
-      <q-btn color="primary" label="login" @click="handleLogin" />
-    </div>
-    <div class="col">
-    </div>
-  </div>
-  <!-- <nut-row class="login-container">
+  <nut-row class="login-container">
     <nut-col class="space"></nut-col>
     <nut-col>
-      <nut-form class="login-form">
+      <nut-form class="login-form" :model-value="formData" ref="ruleForm">
         <nut-form-item prop="username" label="用户名"
           :rules="[{ required: true, message: '请填写用户名' }]">
-          <input class="nut-input-text" @blur="customBlurValidate('name')"
-            v-model="username" placeholder="请输入姓名" type="text" />
+          <input class="nut-input-text" @blur="customBlurValidate('username')"
+            v-model="formData.username" placeholder="请输入姓名" type="text"
+            autocomplete="username" />
         </nut-form-item>
         <nut-form-item prop="password" label="密码"
           :rules="[{ required: true, message: '请填写密码' }]">
           <input class="nut-input-text" @blur="customBlurValidate('password')"
-            v-model="password" placeholder="请输入姓名" type="password" />
+            v-model="formData.password" placeholder="请输入姓名" type="password"
+            autocomplete="password" />
         </nut-form-item>
 
         <nut-cell>
@@ -30,34 +23,40 @@
         </nut-cell>
       </nut-form>
     </nut-col>
-  </nut-row> -->
+  </nut-row>
 </template>
 
 <script>
-import { ref } from 'vue';
+import { ref, reactive } from 'vue';
 import { useUserStore } from '@/stores/user-store'
 
 export default {
   name: 'LoginIndex',
   setup() {
-    const username = ref('');
-    const password = ref('');
+    const formData = reactive({
+      username: '',
+      password: ''
+    });
+
+    const validate = (item) => {
+      console.log(item);
+    };
+
+    const ruleForm = ref(validate);
+
     return {
-      username,
-      password
+      formData, ruleForm
     };
   },
   methods: {
     handleLogin() {
-      useUserStore().login({}).then(() => {
+      useUserStore().login(this.formData).then(() => {
         this.$router.push({ path: this.redirect || '/', query: this.otherQuery })
       })
     },
     customBlurValidate(prop) {
-      ruleForm.value.validate(prop).then(({ valid, errors }) => {
-        if (valid) {
-          console.log('success', formData);
-        } else {
+      this.ruleForm.validate(prop).then(({ valid, errors }) => {
+        if (!valid) {
           console.log('error submit!!', errors);
         }
       });
--- src/pages/profile/index.vue
+++ src/pages/profile/index.vue
@@ -1,12 +1,54 @@
 <template>
-  <div>Profile</div>
+  <div>
+    <nut-row align="center">
+      <nut-col :span="8">
+        <nut-image round class="avatar" :src="avatar" />
+      </nut-col>
+      <nut-col :span="16" class="desc">
+        <div>{{ mobile }}</div>
+      </nut-col>
+    </nut-row>
+
+    <nut-divider />
+
+    <nut-cell-group>
+      <nut-cell title="设置" is-link to="/setting" />
+    </nut-cell-group>
+    <nut-divider />
+
+    <nut-button round block type="danger" @click="handleLogout">
+      退出
+    </nut-button>
+  </div>
 </template>
 
 <script>
+import { useUserStore } from '@/stores/user-store'
+import { mapState } from 'pinia'
 export default {
-  name: 'ProfileIndex'
+  name: "ProfileIndex",
+  computed: {
+    ...mapState(useUserStore, ['mobile', 'avatar'])
+  },
+  methods: {
+    async handleLogout() {
+      await useUserStore().logout()
+      this.$router.push(`/login?redirect=${this.$route.fullPath}`)
+    }
+  }
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+$avatarSize: 4em;
+
+.avatar {
+  width: $avatarSize;
+  height: $avatarSize;
+  margin: 2em 0 0 2em;
+}
+
+.desc {
+  padding: 2em 0 0 2em;
+}
 </style>
