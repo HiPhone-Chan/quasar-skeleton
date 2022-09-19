10c83314f4e0efec99df2bb4d54902090d89eee6
chf <hi.phone.chan@gmail.com>
2022-09-19 16:36:57 +0800

    sort function to router

--- src/router/constant/index.js
+++ src/router/constant/index.js
@@ -2,13 +2,24 @@ const routerModules = import.meta.globEager('./**.js')
 const routers = []
 
 Object.keys(routerModules).forEach(key => {
-  const importRouteres = routerModules[key].default;
-  if (Array.isArray(importRouteres)) {
-    routers.push(...importRouteres)
+  const importRouters = routerModules[key].default;
+  const priority = routerModules[key].priority;
+  if (Array.isArray(importRouters)) {
+    for (const importRouter of importRouters) {
+      routers.push({
+        router: importRouter,
+        priority: priority
+      })
+    }
   } else {
-    routers.push(importRouteres)
+    routers.push({
+      router: importRouters,
+      priority: priority
+    })
   }
 })
 // modules entry
 /** when your routing map is too long, you can split it into small modules **/
-export default routers
+export default routers.sort((ra, rb) => {
+  return ra.priority - rb.priority // 升序
+}).map(item => item.router)
