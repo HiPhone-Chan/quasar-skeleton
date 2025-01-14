e6e4ff8fe8c39a107408c3b45643a565fcd5d83c
chf <hi.phone.chan@gmail.com>
2025-01-14 14:29:42 +0800

    update package version;remove export i18nGlobal

--- src/boot/i18n.js
+++ src/boot/i18n.js
@@ -3,8 +3,6 @@ import { createI18n } from 'vue-i18n'
 import messages from 'src/i18n'
 import { useAppStore } from '@/stores/app-store'
 
-let i18nGlobal
-
 export default defineBoot(({ app, store }) => {
   const appStore = process.env.SERVER ? useAppStore(store) : useAppStore()
   const i18n = createI18n({
@@ -12,9 +10,6 @@ export default defineBoot(({ app, store }) => {
     globalInjection: true,
     messages,
   })
-  i18nGlobal = i18n.global
   // Set i18n instance on app
   app.use(i18n)
 })
-
-export { i18nGlobal as i18n }
