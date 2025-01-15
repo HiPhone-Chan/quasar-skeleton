0a3152052d8041e7fcf58d0f2093cbacecea7722
chf <hiphone.chan@gmail.com>
2025-01-15 14:12:57 +0800

    add android build script

---
+++ script/README.md
@@ -0,0 +1,10 @@
+# android
+
+```
+key store path: my-release-key.keystore
+key alias: app
+pass 123456
+
+keytool -genkey -v -keystore my-release-key.keystore -alias app -keyalg RSA -keysize 2048 -validity 20000
+
+```
---
+++ script/build.bat
@@ -0,0 +1,35 @@
+@echo off
+@REM 终端编码UTF-8
+@chcp 65001
+
+@REM 读取环境变量
+if exist ".env" (
+    for /f "tokens=1,2 delims==" %%a in ('type .env') do (
+        set %%a=%%b
+    )
+)
+
+if exist ".env.local" (
+    for /f "tokens=1,2 delims==" %%a in ('type .env.local') do (
+        set %%a=%%b
+    )
+)
+
+set ANDROID_BUILD_TOOLPATH=%ANDROID_SDK_PATH%/build-tools/%ANDROID_BUILD_VERSION%
+set ZIPALIGN=%ANDROID_BUILD_TOOLPATH%/zipalign.exe
+set APKSIGNER=%ANDROID_BUILD_TOOLPATH%/apksigner.bat
+
+REM Ensure you're in the root project folder
+cd /d %~dp0\..
+@REM 相对路径转绝对路径, %~dp0 为脚本路径
+for %%I in ("%~dp0\../dist/capacitor/android/apk/release") do set BUILD_DIST_PATH=%%~fI
+
+REM Build the Quasar project for production (optional: use a specific environment)
+echo Building Quasar project...
+@REM call npm run build:android
+call npx cross-env APP_ENV=production quasar build -m capacitor -T android
+
+call %ZIPALIGN% -v 4 %BUILD_DIST_PATH%/app-release-unsigned.apk %BUILD_DIST_PATH%/app-release.apk
+call %APKSIGNER% sign --ks %~dp0/my-release-key.keystore --ks-key-alias %KS_KEY_ALIAS% --ks-pass pass:%KS_PASS% %BUILD_DIST_PATH%/app-release.apk
+
+@pause
