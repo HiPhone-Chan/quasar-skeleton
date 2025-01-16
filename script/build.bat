030b15531df8a7a2b908c8660c2964e92a75c82f
chf <hiphone.chan@gmail.com>
2025-01-16 13:51:38 +0800

    update windows build android script

--- script/build.bat
+++ script/build.bat
@@ -1,5 +1,5 @@
 @echo off
-@REM 终端编码UTF-8
+@REM 终端编码UTF-8，注意脚本换行符号应为CRLF
 @chcp 65001
 
 @REM 读取环境变量
@@ -15,20 +15,31 @@ if exist ".env.local" (
     )
 )
 
+@REM 相对路径转绝对路径, %~dp0 为脚本路径
+set SCRIPT_PATH=%~dp0
+for %%I in ("%~dp0/..") do set PROJECT_PATH=%%~fI
 set ANDROID_BUILD_TOOLPATH=%ANDROID_SDK_PATH%/build-tools/%ANDROID_BUILD_VERSION%
 set ZIPALIGN=%ANDROID_BUILD_TOOLPATH%/zipalign.exe
 set APKSIGNER=%ANDROID_BUILD_TOOLPATH%/apksigner.bat
 
 REM Ensure you're in the root project folder
-cd /d %~dp0\..
-@REM 相对路径转绝对路径, %~dp0 为脚本路径
-for %%I in ("%~dp0\../dist/capacitor/android/apk/release") do set BUILD_DIST_PATH=%%~fI
+@REM cd /d %~dp0/..
+cd /d %PROJECT_PATH%
 
 REM Build the Quasar project for production (optional: use a specific environment)
 echo Building Quasar project...
-call npx cross-env APP_ENV=production quasar build -m capacitor -T android
 
-call %ZIPALIGN% -v 4 %BUILD_DIST_PATH%/app-release-unsigned.apk %BUILD_DIST_PATH%/app-release.apk
-call %APKSIGNER% sign --ks %~dp0/my-release-key.keystore --ks-key-alias %KS_KEY_ALIAS% --ks-pass pass:%KS_PASS% %BUILD_DIST_PATH%/app-release.apk
+set BUILD_DIST_PATH=%PROJECT_PATH%/dist/capacitor/android/apk/release
+
+if "%DEBUG%"=="true" (
+    echo Building in debug mode...
+    call npx cross-env APP_ENV=production quasar build -m capacitor -T android -d
+) else (
+    call npx cross-env APP_ENV=production quasar build -m capacitor -T android
+
+    call %ZIPALIGN% -v 4 %BUILD_DIST_PATH%/app-release-unsigned.apk %BUILD_DIST_PATH%/%RELEASE_APK_NAME%
+    call %APKSIGNER% sign --ks %~dp0/my-release-key.keystore --ks-key-alias %KS_KEY_ALIAS% --ks-pass pass:%KS_PASS% %BUILD_DIST_PATH%/%RELEASE_APK_NAME%
+)
+
 
 @pause
